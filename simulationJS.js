5f8a2c145ae16f9bcd948e1390f03604035677f2
UNSW account <unswaccount@anujans-air-2.home>
2023-10-31 15:00:45 +1100

    created a canvas and am beginning to learn how to draw on it

---
+++ simulationJS.js
@@ -0,0 +1,13 @@
+// Big shout out to Chris Courses 
+// https://www.youtube.com/watch?v=EO6OkltgudE&t=213s
+
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight * 3 / 4 ;
+var ctx = canvas.getContext("2d"); // passing a ton of methods through ctx; it's a magic paintbrush
+
+// ctx.fillRect(1, 4, 100, 40) // arguments are x, y, width, height
+
+
+ 
