62c8c594da32cbe28902fcec9d4701c2c50665f0
Anujan Cenan <anujancenan@Anujans-MacBook-Air.local>
2024-12-18 01:10:43 +1100

    wrote the reading pixels functionality to figure out if the pixel i click on is transparent or not – want to do this better by reading the pixel directly off the canvas rather than using a pixel colour file and python script

--- src/processingFunctions/readingPixels.js
+++ src/processingFunctions/readingPixels.js
@@ -1,52 +1,18 @@
-export function clickedOnCannon(ctx, canvas, mouse_x, mouse_y, cannonInfo, angle) {
+import cannonPixelColours from "./cannonV2Pixels.txt"
+export async function clickedOnCannon(ctx, canvas, mouse_x, mouse_y, cannonInfo, angle) {
 
   const [TOP_LEFT_CORNER, v1, v2] = findCannonPointAndPlane(ctx, canvas, cannonInfo, angle);
   
-  console.log(window.devicePixelRatio)
+  console.log(`window device pixel ratio = ${window.devicePixelRatio}`)
   mouse_x *= window.devicePixelRatio;
   mouse_y *= window.devicePixelRatio;
 
-  console.log(`mouse coords = ${mouse_x}, ${mouse_y}`);
-  console.log(`TOP LEFT CORNER COORDS = ${TOP_LEFT_CORNER[0]}, ${TOP_LEFT_CORNER[1]}`);
-
-
-  let x1 = v1[0];
-  let y1 = v1[1];
-  let x2 = v2[0];
-  let y2 = v2[1];
-
-  // if (x1 === 0) x1 = 0.01
-  // if (y1 === 0) y1 = 0.01
-  // if (x2 === 0) x2 = 0.01
-  // if (y2 === 0) y2 = 0.01
-
-  // if x1, x2, y1 or y2 is 0, there will be a division by 0 error
-
-  // mouse_x = TOP_LEFT_CORNER[0] + lambda * x1 + mu * x2   (1)
-  // mouse_y = TOP_LEFT_CORNER[1] + lambda * y1 + mu * y2   (2)
-
-  // mouse_x - TOP_LEFT_CORNER[0] = lambda * x1 + mu * x2   (1) (rearranged)
-  // mouse_y - TOP_LEFT_CORNER[1] = lambda * y1 + mu * y2   (2) (rearranged)
-
-  //  y1 * (mouse_x - TOP_LEFT_CORNER[0]) = y1 * (lambda * x1 + mu * x2)    (1) (multiply y1 to both sides)
-  //  x1 * (mouse_y - TOP_LEFT_CORNER[1]) = x1 * (lambda * y1 + mu * y2)    (2) (multiply x1 to both sides)
-
-  //  y1 * (mouse_x - TOP_LEFT_CORNER[0]) = y1 * lambda * x1 + y1 * mu * x2;  (1) (expand right hand side)
-  //  x1 * (mouse_y - TOP_LEFT_CORNER[1]) = x1 * lambda * y1 + x1 * mu * y2;  (2) (expand right hand side)
-
-
-  // y1 * (mouse_x - TOP_LEFT_CORNER[0]) - x1 * (mouse_y - TOP_LEFT_CORNER[1]) = (y1 * lambda * x1 + y1 * mu * x2) - (x1 * lambda * y1 + x1 * mu * y2)  (3) ( (2) - (1) )
-  // y1 * (mouse_x - TOP_LEFT_CORNER[0]) - x1 * (mouse_y - TOP_LEFT_CORNER[1]) = (y1 * mu * x2) - (x1 * mu * y2)      (collect like terms on the right hand side)
-  // y1 * (mouse_x - TOP_LEFT_CORNER[0]) - x1 * (mouse_y - TOP_LEFT_CORNER[1]) = mu * ((y1  * x2) - (x1  * y2))       (factorise out the mu on the right hand side)
-
-  const mu = (y1 * (mouse_x - TOP_LEFT_CORNER[0]) - x1 * (mouse_y - TOP_LEFT_CORNER[1])) / ((y1  * x2) - (x1  * y2))    // make mu the subject
-
-  //  (mouse_x - TOP_LEFT_CORNER[0] -  mu * x2) / x1  = lambda      (rearranging (1) to make lambda the subject )
-
-  const lambda =  (mouse_x - TOP_LEFT_CORNER[0] -  mu * x2) / x1;
-
-  const proposedX = TOP_LEFT_CORNER[0] + lambda * x1 + mu * x2;
-  const proposedY = TOP_LEFT_CORNER[1] + lambda * y1 + mu * y2;
+  const [lambda, mu] = calculateLambdaAndMu(TOP_LEFT_CORNER, v1[0], v1[1], v2[0], v2[1], mouse_x, mouse_y);
+  if (lambda < 0 || lambda > 1 || mu < 0 || mu > 1) {
+    return false;
+  }
+  const proposedX = TOP_LEFT_CORNER[0] + lambda * v1[0] + mu * v2[0];
+  const proposedY = TOP_LEFT_CORNER[1] + lambda * v1[1] + mu * v2[1];
 
 
   ctx.clearRect(0,0,canvas.width, canvas.height * 0.1)
@@ -54,35 +20,30 @@ export function clickedOnCannon(ctx, canvas, mouse_x, mouse_y, cannonInfo, angle
   ctx.fillText(`TOP LEFT CORNER = ${TOP_LEFT_CORNER[0]}, ${TOP_LEFT_CORNER[1]} \n lambda, mu = ${lambda}, ${mu}`,10,80);
 
 
-  console.log(`proposed Coords are ${proposedX}, ${proposedY}`)
-
   ctx.beginPath();
   ctx.arc(proposedX, proposedY, 15, 0, 2 * Math.PI);
   ctx.strokeStyle = "red";
   ctx.stroke();
 
+  const rowsPassed = Math.round(mu * cannonInfo.pixel_height);
+  const columnsPassed = Math.round(lambda * cannonInfo.pixel_width);
 
+  let transparency = false;
 
+  /////// better way to do this is to use the ctx variable to check the pixel colour
+  await fetch(cannonPixelColours)
+  .then(r => r.text())
+  .then(text => {
+    const wholeFile = (text.split('\n'));
 
+    if (wholeFile[rowsPassed * cannonInfo.pixel_width - 1 + columnsPassed] === "0, 0, 0, 0") {
+      transparency = true;
+    }
+ });
 
-  console.log(`pixel height of cannon is ${cannonInfo.pixel_height}`)
-  console.log(`pixel width of cannon is ${cannonInfo.pixel_width}`)
+ console.log(`transparency = ${transparency}`);
 
-  console.log(`fraction of height of cannon is ${cannonInfo.pixel_height * mu}`)
-
-
-  const pixel_num = (cannonInfo.pixel_height * mu * cannonInfo.pixel_width) + lambda * cannonInfo.pixel_width;
-  console.log(Math.round(pixel_num))
-
-
-    // if we didnt even click on the cannon Image
-    //      return false
-
-    // if the pixel, mapped to the original image is transparent
-    //      return false
-
-    // return true
-
+ return !transparency;
 }
 
 function findCannonPointAndPlane(ctx, canvas, cannonInfo, angle) {
@@ -127,4 +88,33 @@ function findCannonPointAndPlane(ctx, canvas, cannonInfo, angle) {
     (cannonInfo.pixel_height * growthFactor) * Math.sin(Math.PI / 2 - angle_rad)
   ]
   return [TOP_LEFT_CORNER, vector1, vector2]
+}
+
+function calculateLambdaAndMu(TOP_LEFT_CORNER, x1, y1, x2, y2, mouse_x, mouse_y) {
+
+  // if x1, x2, y1 or y2 is 0, there will be a division by 0 error
+
+  // mouse_x = TOP_LEFT_CORNER[0] + lambda * x1 + mu * x2   (1)
+  // mouse_y = TOP_LEFT_CORNER[1] + lambda * y1 + mu * y2   (2)
+
+  // mouse_x - TOP_LEFT_CORNER[0] = lambda * x1 + mu * x2   (1) (rearranged)
+  // mouse_y - TOP_LEFT_CORNER[1] = lambda * y1 + mu * y2   (2) (rearranged)
+
+  //  y1 * (mouse_x - TOP_LEFT_CORNER[0]) = y1 * (lambda * x1 + mu * x2)    (1) (multiply y1 to both sides)
+  //  x1 * (mouse_y - TOP_LEFT_CORNER[1]) = x1 * (lambda * y1 + mu * y2)    (2) (multiply x1 to both sides)
+
+  //  y1 * (mouse_x - TOP_LEFT_CORNER[0]) = y1 * lambda * x1 + y1 * mu * x2;  (1) (expand right hand side)
+  //  x1 * (mouse_y - TOP_LEFT_CORNER[1]) = x1 * lambda * y1 + x1 * mu * y2;  (2) (expand right hand side)
+
+
+  // y1 * (mouse_x - TOP_LEFT_CORNER[0]) - x1 * (mouse_y - TOP_LEFT_CORNER[1]) = (y1 * lambda * x1 + y1 * mu * x2) - (x1 * lambda * y1 + x1 * mu * y2)  (3) ( (2) - (1) )
+  // y1 * (mouse_x - TOP_LEFT_CORNER[0]) - x1 * (mouse_y - TOP_LEFT_CORNER[1]) = (y1 * mu * x2) - (x1 * mu * y2)      (collect like terms on the right hand side)
+  // y1 * (mouse_x - TOP_LEFT_CORNER[0]) - x1 * (mouse_y - TOP_LEFT_CORNER[1]) = mu * ((y1  * x2) - (x1  * y2))       (factorise out the mu on the right hand side)
+
+  const mu = (y1 * (mouse_x - TOP_LEFT_CORNER[0]) - x1 * (mouse_y - TOP_LEFT_CORNER[1])) / ((y1  * x2) - (x1  * y2))    // make mu the subject
+
+  //  (mouse_x - TOP_LEFT_CORNER[0] -  mu * x2) / x1  = lambda      (rearranging (1) to make lambda the subject )
+
+  const lambda =  (mouse_x - TOP_LEFT_CORNER[0] -  mu * x2) / x1;
+  return [lambda, mu]
 }
