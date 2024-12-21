6587027286dc1504c2f6d741a26d974ef7cfb99a
BobHell0 <bokchoyjunior@gmail.com>
2024-12-21 12:29:45 +1100

    contains the function that finds the iniital position of the cannon ball

---
+++ src/processingFunctions/findFrontOfCannon.js
@@ -0,0 +1,14 @@
+export function findFrontOfCannon(canvas, angle, cannonInfo) {
+  const pivX = canvas.width * cannonInfo.scalar_top_corner_x + 
+    cannonInfo.pivot_x * cannonInfo.growth_factor
+
+  const pivY = canvas.height * cannonInfo.scalar_top_corner_y + 
+    cannonInfo.pivot_y * cannonInfo.growth_factor
+  
+  const magnitude = cannonInfo.growth_factor * (cannonInfo.pixel_width - cannonInfo.pivot_x);
+
+  const angle_rad = angle * (Math.PI / 180)
+  const vector = [magnitude * Math.cos(angle_rad), -magnitude * Math.sin(angle_rad)];
+
+  return [pivX + vector[0], pivY + vector[1]]
+}
