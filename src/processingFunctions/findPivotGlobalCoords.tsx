dae97b5d3c3950212e1f5f1989c1c6c507a3f592
BobHell0 <anujan.cenan@gmail.com>
2025-01-22 09:44:49 +1100

    deleted the deprecated files

--- src/processingFunctions/findPivotGlobalCoords.tsx
+++
@@ -1,7 +0,0 @@
-// export function findPivotGlobalCoords(canvas: HTMLCanvasElement, USER_ANCHOR_POINT: number[]) {
-//   const pivX = window.innerWidth * USER_ANCHOR_POINT[0] * window.devicePixelRatio;
-
-//   const pivY = canvas.height * USER_ANCHOR_POINT[1]
-
-//   return [pivX, pivY]
-// }
