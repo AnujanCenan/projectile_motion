e921ce2bc3677443b66bb1bff03203034633c61d
BobHell0 <anujan.cenan@gmail.com>
2025-01-21 23:18:07 +1100

    added a velocityInfo type

--- src/types/jsonTypes.tsx
+++ src/types/jsonTypes.tsx
@@ -11,3 +11,10 @@ interface HolsterInfo {
   pivot_x: number;
   pivot_y: number;  
 }
+
+interface VelocitySliderInfo {
+  pixel_width: number;
+  pixel_height: number;
+  slider_pixel_width: number;
+  slider_pixel_height: number;  
+}
