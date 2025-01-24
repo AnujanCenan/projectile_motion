4ee9c52db72b66dcd2c1b13a06af97ce83b3d954
BobHell0 <anujan.cenan@gmail.com>
2025-01-24 11:46:05 +1100

    rename for uniformity

---
+++ src/states/tutorialStates/DraggingHeightArrowInstructions.tsx
@@ -0,0 +1,12 @@
+import { TutorialState } from "./TutorialState";
+
+
+export class DragHeightArrowInstructions extends TutorialState {
+  completeDialogue(): TutorialState {
+    throw new Error("Method not implemented.");
+  }
+  completedTask(): TutorialState {
+    throw new Error("Method not implemented.");
+  }
+
+}
