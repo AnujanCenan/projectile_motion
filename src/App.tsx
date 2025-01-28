b53226d232471f48acdaefadc2c00334f62e6a4e
BobHell0 <bokchoyjunior@gmail.com>
2025-01-28 15:44:40 +1100

    replaced the tutorial with the level selection (for testing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import LevelSelection from './components/levelSelection/levelSelection.tsx';
 import Tutorial from './components/tutorial/Tutorial.tsx';
 
 function App() {
   
   return (
-    <Tutorial/>
+    <LevelSelection levels={["Tutorial", "Level 1", "Level 2", "Level 3", "Level 4", "Level 5"]} />
   );
 }
 
