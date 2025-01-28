221bbc258a1cb3b6ef877dfbff31548377344e85
BobHell0 <bokchoyjunior@gmail.com>
2025-01-28 22:00:16 +1100

    figured out that we can use a state variable to store the desired component for App.tsx to return

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import LevelSelection from './components/levelSelection/LevelSelection';
 import levelsFile from "./components/levelSelection/levels.json"
+import Tutorial from './components/tutorial/Tutorial';
 function App() {
+  const [projectileMotionPage, setProjectileMotionPage] = useState(<LevelSelection levels={levelsFile.levels} />);
 
-  return (
-    <LevelSelection
-      levels={levelsFile.levels}
-    />
-  )
+  return projectileMotionPage
 }
 export default App;
