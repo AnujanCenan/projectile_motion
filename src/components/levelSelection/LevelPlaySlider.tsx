c3b2ba71b188fe71732bebba5f1bcb9e23b6511a
BobHell0 <bokchoyjunior@gmail.com>
2025-01-28 22:01:47 +1100

    constructing the slider to cover the level selection container completely

--- src/components/levelSelection/LevelPlaySlider.tsx
+++ src/components/levelSelection/LevelPlaySlider.tsx
@@ -7,14 +7,20 @@ interface LevelPlayerSliderProps {
 export default function LevelPlaySlider({level, setLevelFocus}: LevelPlayerSliderProps) {
   return (
     <>
-      <div id="LevelPlaySlider_container" onClick={() => setLevelFocus(null)}>
-        <div id="LevelPlaySlider_title">{level.name}</div>
-        <div className="flexBreak"/>
-        <div id="LevelPlaySlider_blurb">{level.blurb}</div>
-        <div className="flexBreak"/>
-        <div id="LevelPlaySlider_description">{level.description}</div>
-        <button id="LevelPlaySlider_cancelButton">Cancel</button>
-        <button id="LevelPlaySlider_playButton">Begin Mission</button>
+      <div id="LevelPlaySlider_container">
+        <div id="LevelPlaySlider_textContainer" onClick={() => setLevelFocus(null)}>
+          <div id="LevelPlaySlider_title">{level.name}</div>
+          <div className="flexBreak"/>
+          <div id="LevelPlaySlider_blurb">{level.blurb}</div>
+          <div className="flexBreak"/>
+          <div id="LevelPlaySlider_description">{level.description?.map((line) => <>{line}<br/><br/></>)}</div>
+          
+          <div id="LevelPlaySlider_buttonContainer">
+            <button id="LevelPlaySlider_cancelButton">Cancel</button>
+            <button id="LevelPlaySlider_playButton">Begin Mission</button>
+          </div>
+
+        </div>
       </div>
     </>
   )
