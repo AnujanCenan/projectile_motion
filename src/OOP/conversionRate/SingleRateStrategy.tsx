085d6f58fcc38431b739f47de59915b95a9a197c
BobHell0 <anujan.cenan@gmail.com>
2025-02-19 11:56:35 +1100

    made conversion rate a straight 5

--- src/OOP/conversionRate/SingleRateStrategy.tsx
+++ src/OOP/conversionRate/SingleRateStrategy.tsx
@@ -2,9 +2,11 @@ import { CanvasPositionAndSizes } from "../CanvasPositionAndSizes";
 
 export class SingleRateStrategy {
   calculateConversionRateXDirection(positionAndSizes: CanvasPositionAndSizes, USER_ANCHOR_POINT: number[]): number {
-    const availableSpace = (positionAndSizes.getCanvas().width - positionAndSizes.getPivotX(USER_ANCHOR_POINT[0])) * 9/10;
-    const conversionRate = availableSpace / positionAndSizes.getMaxHorizontalRage();
-    return conversionRate 
+    // const availableSpace = (positionAndSizes.getCanvas().width - positionAndSizes.getPivotX(USER_ANCHOR_POINT[0])) * 9/10;
+    // const conversionRate = availableSpace / positionAndSizes.getMaxHorizontalRage();
+    // return conversionRate 
+
+    return 5;
   }
 
   calculateConversionRateYDirection(positionAndSizes: CanvasPositionAndSizes, USER_ANCHOR_POINT: number[]): number {
